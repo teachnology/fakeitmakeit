9142e92487765c254f3102b312db33d008119d1c
marijanbeg <m.beg@imperial.ac.uk>
2023-11-08 16:34:54 +0000

    EOD commit.

--- phantombunch/util.py
+++ phantombunch/util.py
@@ -1,3 +1,4 @@
+from faker import Faker
 import random
 
 
@@ -7,11 +8,15 @@ def cid():
     The first digit is always 0, whereas the second digit is randomly 1 or 2. The remaining 6 digits are randomly generated between 0 and 9.
 
     """
-    # The first digit is always 0
+    # The first digit is always 0.
     number = '0'
-    # The second digit is randomly 1 or 2
+    # The second digit is randomly 1 or 2.
     number += str(random.choice([1, 2]))
-    # Generate the remaining 6 digits randomly between 0 and 9
-    for _ in range(6):
-        number += str(random.randint(0, 9))
+    # Generate the remaining 6 digits randomly between 0 and 9.
+    number += ''.join(str(random.randint(0, 9)) for _ in range(6))
+    
     return number
+
+
+def gender():
+    pass
